2d53ff62e01242f323e5dbfcf97b38cb1691ffdc
Willem Verbeke <willem_verbeke@hotmail.com>
2024-03-12 22:31:27 +0100

    Start dataset building

--- build_dataset/make_webtext.py
+++ build_dataset/make_webtext.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
 from tqdm import tqdm
+from tokenizer import END_OF_TEXT
 
 dataset = load_dataset("Skylion007/openwebtext")
 dataset = dataset["train"]
@@ -7,5 +8,8 @@ dataset = dataset["train"]
 with open("webtext.txt", "w") as f:
     for d in tqdm(dataset):
         d = d["text"]
-        f.write(d + "<|endoftext|>\n")
+
+        # Should a newline be added here as well or is end of text enough?
+        # Feels like we can fit more content without a redundant newline.
+        f.write(d + END_OF_TEXT)
 
